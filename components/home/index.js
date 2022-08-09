ef31469b7d179bbfceb24de61a5dcdf4ce02a2b2
Manish <manish@Manishs-MacBook-Air.local>
2022-08-10 00:14:22 +0530

    resume dynamic

--- components/home/index.js
+++ components/home/index.js
@@ -17,7 +17,7 @@ const HomePage = () => {
           <WordCloud />
           <Projects />
           <Contact />
-          <Repo />
+          {/* <Repo /> */}
         </div>
       </div>
     </>
